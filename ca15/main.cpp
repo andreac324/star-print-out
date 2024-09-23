96ed62bc89a581ad1c7faaa874b0d7ab93ffcb38
andreac324 <terisa32873@gmail.com>
2024-09-23 11:15:00 -0700

    Initial Commit

---
+++ ca15/main.cpp
@@ -0,0 +1,23 @@
+//
+//  main.cpp
+//  ca15
+//
+//  Created by Andrea d Cazares on 9/23/24.
+//
+
+#include <iostream>
+using namespace std;
+int main(){
+    for (int i=0; i<5;i++){
+        cout<< " \n";
+        for (int j= 0; j<=i;j++){
+        cout<< "*";
+
+        }
+        
+    }
+    cout<< "\n*"<<endl;
+    return 0;
+
+    
+}
